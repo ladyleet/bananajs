38d2965140f08d571589b3cc447e9a53ce706773
Tracy Lee <tracy@wingman.events>
2016-09-21 21:13:27 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav>
+    <div class="nav-wrapper deep-orange">
+      <a href="#" class="brand-logo center"><img src="/assets/banana-logo.png"/></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">temp</a></li>
+        <li><a href="#">temp</a></li>
+        <li><a href="#">temp</a></li>
+      </ul>
+    </div>
+  </nav>
+`
 })
 export class AppComponent {
   title = 'app works!';
