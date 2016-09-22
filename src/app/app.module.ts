6f38aab9be830ccb4e167f262fe72ad5ab5f9ff4
Tracy Lee <tracy@wingman.events>
2016-09-21 22:14:05 -0400

    teaching ben

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { BananasComponent } from './bananas/bananas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    BananasComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ appRoutingProviders ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
