b5af096141c3ee1d0edb22d9ddbbe9c142fc7dad
Tracy Lee <tracy@wingman.events>
2016-10-02 16:48:53 -0400

    run at svcc

---
+++ src/app/get-bananas/get-bananas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-bananas',
+  templateUrl: './get-bananas.component.html',
+  styleUrls: ['./get-bananas.component.css']
+})
+export class GetBananasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
